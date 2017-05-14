8d35f320bbd9ca9bf4a750021e9296e25d63fce2
PeterPan <51656281@qq.com>
2017-05-14 13:36:46 +0800

    no message

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace app\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\WxLoading;
 use App\Libs\RequestHandel;
 use app\Model\TestModel;
 use Illuminate\Http\Request;
@@ -23,6 +24,8 @@ class AdminController extends Controller
         $url = 'https://login.weixin.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Flogin.weixin.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=zh_CN&_='.$this->TurnTime;
         $queue = new RequestHandel($url);
         $res = $queue->request(array(),'GET',0,array('window.QRLogin.code = 200; window.QRLogin.uuid = "'=>'','";'=>''),0,'body');
+
+        dispatch(new WxLoading());
 
         echo $res;
     }
