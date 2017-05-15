34e272c8404dbb233f6f3fdd4c4e067c62775ac1
Lee <51656281@qq.com>
2017-05-15 03:06:33 +0000

    l

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,14 +24,21 @@ class AdminController extends Controller
     {
         $value = $request->input('value','ddd');
         $url = 'https://login.weixin.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Flogin.weixin.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=zh_CN&_='.$this->TurnTime;
-        // $queue = new RequestHandel($url);
-        // $res = $queue->request(array(),'GET',0,array('window.QRLogin.code = 200; window.QRLogin.uuid = "'=>'','";'=>''),0,'body');
+        $queue = new RequestHandel($url);
+        $res = $queue->request(array(),'GET',0,array('window.QRLogin.code = 200; window.QRLogin.uuid = "'=>'','";'=>''),0,'body');
         // event(new WxMessage($value));
-        if($value != 'ddd'){
-            dispatch(new WxLoading(''));
-        }
+        // if($value != 'ddd'){
+        //     dispatch(new WxLoading(''));
+        // }
 
-        $res = Redis::sMembers('wxduilieceshi');
+        // $res = Redis::sMembers('wxduilieceshi');
+        if(strpos($res,'window.QRLogin.code = 200; window.QRLogin.uuid = "')){
+        	$arr[0] = $res;
+        	$arr[1] = strstr($arr[0], 'window.QRLogin.code = 200; window.QRLogin.uuid = "');
+        	$arr[2] = substr($arr[1], 50,12);
+        	var_dump($arr);
+        	exit();
+        }
         var_dump($res);
     }
 
