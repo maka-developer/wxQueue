67e7632d3120d1c35d35749134023b542bb5a33d
PeterPan <51656281@qq.com>
2017-05-16 22:27:33 +0800

    no message

--- app/Libs/WxGetItem.php
+++ app/Libs/WxGetItem.php
@@ -20,12 +20,8 @@ class WxGetItem
         $url = 'https://login.weixin.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Flogin.weixin.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=zh_CN&_='.$TurnTime;
         $queue = new RequestHandel($url);
         $res = $queue->request(array(),'GET',0, 0,'body');
-        if(strpos($res,'window.QRLogin.code = 200; window.QRLogin.uuid = "')){
-            $uuid = substr(strstr($res, 'window.QRLogin.code = 200; window.QRLogin.uuid = "'), 50, 12);
-            Redis::set(config('rkey.uuid.key'), $uuid);
-            Redis::set(config('rkey.code.key'), 0);
-        }else{
-            Redis::hset(config('rkey.testMsg.key'), date('Y-m-d H:i:s'), $res);
-        }
+        $uuid = substr($res, 50, 12);
+        Redis::set(config('rkey.uuid.key'), $uuid);
+        Redis::set(config('rkey.code.key'), 0);
     }
 }
