58b21406b96a4002792b73726f4131256057fed3
Lee <51656281@qq.com>
2017-06-01 16:11:10 +0800

    no message

--- config/queue.php
+++ config/queue.php
@@ -61,7 +61,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => 'default',
-            'retry_after' => 1,     //设置队列死掉重新执行的时间
+            'retry_after' => 1,
         ],
 
     ],
