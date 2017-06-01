67cfbf52b1abb4667dd2c8a94119fee864689ede
Lee <51656281@qq.com>
2017-06-01 16:13:41 +0800

    Revert "no message"
    This reverts commit 2c8bfb7ce9ab429fa763be22dd82e298825497bc.

--- config/queue.php
+++ config/queue.php
@@ -61,7 +61,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => 'default',
-            'retry_after' => 1,
+            'retry_after' => 0,
         ],
 
     ],
