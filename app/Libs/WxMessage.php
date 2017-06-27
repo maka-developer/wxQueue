f27f8909a06568c60e67cbc3401979769f9c0f87
Lee <51656281@qq.com>
2017-06-27 15:22:43 +0800

    no message

--- app/Libs/WxMessage.php
+++ app/Libs/WxMessage.php
@@ -54,8 +54,6 @@ class WxMessage
         if(!$body['AddMsgList']){
             exit();
         }
-        echo 1;
-        exit();
         foreach($body['AddMsgList'] as $key=>$value){
             if($value['Content'] == ''){
                 continue;
@@ -64,22 +62,24 @@ class WxMessage
                 //判断是否群组
             if(strstr($value['FromUserName'],'@@')){    //群组，判断有无指令  set:
                 //首先判断是否设置指令
-                $group = GroupModel::where('UserName',$from)->first();
-                if($group[0] == null) {  //未设置命令
-                    if ($str = strstr($value['Content'], 'set:')) {
-                        $content = explode($str, ':');
-                        $content = $content[1];         //指令
-                        $group = new GroupModel();
-                        $group['UserName'] = $value['FromUserName'];
-                        $group['instructions'] = $content;
-                        $group->save();
-                    } else {
-                        exit();
-                    }
-                }else{
-                    self::sendMsg($value['FromUserName'], '命令已经设置，无需重复设置');
-                    exit();
-                }
+                echo 2;
+                exit();
+//                $group = GroupModel::where('UserName',$from)->first();
+//                if($group[0] == null) {  //未设置命令
+//                    if ($str = strstr($value['Content'], 'set:')) {
+//                        $content = explode($str, ':');
+//                        $content = $content[1];         //指令
+//                        $group = new GroupModel();
+//                        $group['UserName'] = $value['FromUserName'];
+//                        $group['instructions'] = $content;
+//                        $group->save();
+//                    } else {
+//                        exit();
+//                    }
+//                }else{
+//                    self::sendMsg($value['FromUserName'], '命令已经设置，无需重复设置');
+//                    exit();
+//                }
             }else{  //没有好友信息
                 exit();
             }
